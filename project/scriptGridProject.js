82bda70b8050a2be1a7475775c77de6a527bc56d
Anicolas Rafamelantsoa <121011007+AnicolasRafamelantsoa@users.noreply.github.com>
2026-03-23 12:03:14 +0300

    Add files via upload

---
+++ project/scriptGridProject.js
@@ -0,0 +1,43 @@
+
+
+function openWindowMirasa(){
+    window.open("association_mirasa.html","_parent");
+}
+
+function openWindowBeach(){
+    window.open("Dola_Beach.html","_parent");
+}
+
+ function openWindowMandroal(){
+     window.open("mandroal.html","_parent");
+ }
+
+ function openWindowRufus(){
+     window.open("rufus.html","_parent");
+ }
+
+ function openWindowNavix(){
+     window.open("navix.html","_parent");
+ }
+
+ function openWindowTragnorek(){
+     window.open("tragnorek.html","_parent");
+ }
+
+ function openWindowShako(){
+     window.open("shako.html","_parent");
+ }
+
+ function openWindowDixi(){
+     window.open("dixi.html","_parent");
+ }
+
+ function openWindowLongoza(){
+     window.open("longoza.html","_parent");
+ }
+
+ function openWindowAsyf(){
+     window.open("asc_asyf.html","_parent");
+ }
+
+
